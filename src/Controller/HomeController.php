1f1cc1c6eaf8b6c16b92f786e2381e5cb157f3d9
ryansujana <ryan.sujana25@gmail.com>
2021-06-20 23:41:14 +0700

    Header, Footer on templates and IF Condition

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,24 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-          class HomeController extends AbstractController
-          {
+class HomeController extends AbstractController
+{
           /**
           * @Route("/", name="RouteName")
           */
           public function home()
           {
-                    return $this->render('index.html.twig',[
-                              "var" => "Memang ganteng"
-                    ]);
+               $tabs = [1, 2, 3, 4, 5]; 
+               $bool = true;
+               return $this->render('index.html.twig',[
+                    "var"     => "Memang ganteng",
+                    'tabs'    => $tabs,
+                    "bool"    => $bool
+               ]);
           }
 
-          /**
+               /**
           * @Route("/params/{name}", name="name", defaults={"name": "Suwandi"}, methods={"GET"})
           */
-          public function params(string $name)
-          {
+               public function params(string $name)
+               {
                     return new Response( "Nama : $name" );
-          }
-          
+               }
+
           }
