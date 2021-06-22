7d3b23df23a1978b70f1e2565d4742b00d2d721f
ryansujana <ryan.sujana25@gmail.com>
2021-06-22 21:07:35 +0700

    Membuat Form

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+	/**
+	 * @Route("/article/new", name="article_new")
+	 * @param Request $request
+	 * @return Response
+	 */
+	public function new(Request $request): Response
+	{
+		return $this->render('article/new.html.twig');
+	}
+}
